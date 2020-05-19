1714035c3016328bdb7a1ec653eb4b23236d70bb
new-HyeonWoo <chlgusdn3@naver.com>
2020-05-19 20:41:59 +0900

    리엑티브 프로그래밍 1

---
+++ src/main/java/toby/reactive/ReactiveStreams_1.java
@@ -0,0 +1,44 @@
+package toby.reactive;
+
+import java.util.Observable;
+import java.util.Observer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SuppressWarnings("deprecation")
+public class ReactiveStreams_1 {
+
+    static class IntObservable extends Observable implements Runnable {
+
+        @Override
+        public void run() {
+            for(int i=0; i<=10; i++) {
+                setChanged();
+                notifyObservers(i);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Observer ob = new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+                System.out.println(Thread.currentThread().getName() + " " + arg);
+;            }
+        };
+
+
+        IntObservable io = new IntObservable();
+        io.addObserver(ob);
+
+        ExecutorService es = Executors.newSingleThreadExecutor();
+        es.execute(io);
+
+        System.out.println(Thread.currentThread().getName() + " EXIT");
+        es.shutdown();
+
+    }
+
+}
